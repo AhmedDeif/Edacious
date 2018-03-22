10fbd99937b164ec9fe0f71febb71b2450a92109
Doola <ahmed.adel.abodeif@gmail.com>
2018-03-22 15:15:01 +0200

    Splash screen constraints

--- IOS App/Edacious/Edacious/ViewController.swift
+++ IOS App/Edacious/Edacious/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sloganLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //  Set color of string "food lovers" to red
+        let attributedString: NSMutableAttributedString = NSMutableAttributedString(string: self.sloganLabel.text!);
+        attributedString.setColorForStr(textToFind: "food lovers", color: UIColor(red:  237.0 / 255.0, green: 56.0 / 255.0, blue: 81.0 / 255.0, alpha: 1.0));
+        self.sloganLabel.attributedText = attributedString;
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +28,50 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+
+extension NSMutableAttributedString {
+    
+    // Sets color of textToFind in a String to given color.
+    func setColorForStr(textToFind: String, color: UIColor) {
+        let range = self.mutableString.range(of: textToFind, options:NSString.CompareOptions.caseInsensitive);
+        if range.location != NSNotFound {
+            self.addAttribute(NSAttributedStringKey.foregroundColor, value: color, range: range);
+        }
+        
+    }
+}
+
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
 }
 
