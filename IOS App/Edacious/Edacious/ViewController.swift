e9c7aef7cdaeaed444ba12b5928e308af90327a4
Doola <ahmed.adel.abodeif@gmail.com>
2018-03-22 13:30:04 +0200

    Creating folder tree

---
+++ IOS App/Edacious/Edacious/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Edacious
+//
+//  Created by Ahmed Abodeif on 3/22/18.
+//  Copyright © 2018 Ahmed Abodeif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
